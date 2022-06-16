3027a8bd155c257be8fd2b7d48e4edba9c684b24
Harjit Karmacharya <hkarmacharya@gmail.com>
2022-06-16 16:02:16 +0300

    Functional components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,102 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router,
+         Route, 
+         NavLink, 
+         Routes 
+        } from 'react-router-dom';
 import './App.css';
+import Sample from './components/Sample'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-       ipaddress: '',
-       ipcity: '',
-       ipcountry:'',
-       posts: []
-      //  jsonIP: []
-    };
-  }
-
-  componentDidMount() {
-    this.get()  
-  }
-
-  get = () => {
-    axios.get('/app')
-      .then((res) => {
-        const data = res.data;
-        console.log(data);
-        this.setState({ 
-          posts:      data,
-          ipaddress:  data.ipaddress,
-          ipcity:     data.city,
-          ipcountry:  data.country_code
-        })
-      })
-      .catch(() => {
-        console.log('Error retrieving data')
-      })
-  }
-
-  displayData = (posts) => {
-    if (!posts.length) return null;
-
-    return posts.map((post, index) => (
-      <div key={index} className='data'>
-        <p>{post.ipaddress}</p>
-        <p>{post.ipcity}</p>
-        <p>{post.ipcountry}</p>
-        <button onClick={() => this.deleteData(post._id)}>Delete</button>
-      </div>
-    ));
-  }
-
-  fetchIP = async (event) => {
-    event.preventDefault();
-    const ipdata = await axios.get('https://geolocation-db.com/json/')
-    const options = {
-        clickedData: true,
-        ipaddress: ipdata.data.IPv4,
-        ipcity: ipdata.data.city,
-        ipcountry: ipdata.data.country_name
-    }
-
-    axios({
-      url: '/app/submit',
-      method: 'POST',
-      data: options
-    }).then(res => {
-      console.log("Data sent to server");
-      console.log(res);
-      this.get()
-    }).catch(err => {
-      console.log("internal server error");
-    })
-  }
-
-  deleteData = async (id) => {
-      const response = await axios.delete(`/app/${id}`)
-      if(response.status === 200) {
-        console.log("Deleted " + id)
-        this.get()
-      }
-  }
-  
   render() {
     return (
       <div className="App">
-          <div>
-            <button onClick={this.fetchIP}>Send to DB</button>
-            <div className='table'>{this.displayData(this.state.posts)}</div>
-          </div>
-          <label>
-            <input type="checkbox"/>
-            <span className="menu">
-              <span className="hamburger"></span>
-            </span>
-            <ul>
-              <li> <a href="#">Home</a> </li>
-              <li> <a href="#">About</a> </li>
-              <li> <a href="#">Contact</a> </li>
-            </ul>
-          </label>
+        <div>
+          <Router>
+            <NavLink to="/sample">Sample</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <Routes>
+              <Route path='/sample' element={<Sample/>} />
+            </Routes>
+          </Router>
+        </div>
       </div>
     )
   } 
---
+++ client/src/components/Sample.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+const Sample = () => {
+    const [ipaddress, setIpaddress] = useState('');
+    const [ipcity, setIpcity] = useState('');
+    const [ipcountry, setIpcountry] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        get()
+    }, [])
+
+    const get = () => {
+        axios.get('/app')
+          .then((res) => {
+            const data = res.data;
+            setIpaddress(data.ipaddress)
+            setIpcity(data.city)
+            setIpcountry(data.country_code)
+            setPosts(data)
+          })
+          .catch(() => {
+            console.log('Error retrieving data')
+          })
+    }
+
+    const displayData = (posts) => {
+        if (!posts.length) return null;
+
+        return posts.map((post, index) => (
+          <div key={index} className='data'>
+            <p>{post.ipaddress}</p>
+            <p>{post.ipcity}</p>
+            <p>{post.ipcountry}</p>
+            <button onClick={() => deleteData(post._id)}>Delete</button>
+          </div>
+        ));
+    }
+
+    const fetchIP = async (event) => {
+        event.preventDefault();
+        const ipdata = await axios.get('https://geolocation-db.com/json/')
+        const options = {
+            clickedData: true,
+            ipaddress: ipdata.data.IPv4,
+            ipcity: ipdata.data.city,
+            ipcountry: ipdata.data.country_name
+        }
+
+        axios({
+          url: '/app/submit',
+          method: 'POST',
+          data: options
+        }).then(res => {
+          console.log("Data sent to server");
+          console.log(res);
+          this.get()
+        }).catch(err => {
+          console.log("internal server error");
+        })
+    }
+
+    const deleteData = async (id) => {
+        const response = await axios.delete(`/app/${id}`)
+        if(response.status === 200) {
+            console.log("Deleted " + id)
+            this.get()
+        }
+    }
+  
+  return (
+    <div>
+        <div>
+            <button onClick={fetchIP}>Send to DB</button>
+            <div className='table'>{displayData(posts)}</div>
+        </div>
+    </div>
+  )
+}
+
+export default Sample
