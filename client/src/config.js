ff998551f71277221fdb59163f606941bd532c7d
t4kaha00 <hkarmacharya@gmail.com>
2022-06-27 16:35:24 +0300

    axiosInstance changed from localhost to website

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://mern-stack-trial.herokuapp.com/"
-    baseURL: "http://localhost:5000"
+    baseURL: "https://mern-stack-trial.herokuapp.com/"
 })
